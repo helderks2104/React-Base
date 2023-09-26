2fcf03742b1cf9b911e347012511c78d8dfe2d0b
helder <helderks2104@hotmail.com>
2023-09-25 21:24:34 -0300

    Adicionado react toastify, estilizado o react-roastify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import GlobalStyle from './styles/GlobalStyles';
+import { ToastContainer } from 'react-toastify';
+import GlobalStyle, { Container } from './styles/GlobalStyles';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Routes from './routes';
@@ -13,6 +14,7 @@ export default function App() {
       <Routes />
       <Footer />
       <GlobalStyle />
+      <ToastContainer autoClose={3000} className="toast-container" />
     </Router>
   );
 }
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,7 +1,13 @@
 import styled, { createGlobalStyle } from 'styled-components';
-import { infoColor, primaryColor, primaryDarkColor } from '../config/colors';
+import * as colors from '../config/colors';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default createGlobalStyle`
+    :root{
+  --toastify-icon-color-success: white;
+  --toastify-icon-color-error: white;
+}
+
     * {
         padding: 0;
         margin: 0;
@@ -11,8 +17,8 @@ export default createGlobalStyle`
 
     body {
         font-family: sans-serif;
-        color: ${primaryDarkColor};
-        background-color: ${primaryDarkColor};
+        color: ${colors.primaryDarkColor};
+        background-color: ${colors.primaryDarkColor};
     }
 
     html,body, #root {
@@ -21,7 +27,7 @@ export default createGlobalStyle`
 
     button {
         cursor: pointer;
-        background-color:${primaryColor};
+        background-color:${colors.primaryColor};
         border: none;
         padding: 10px 20px;
         color: #fff;
@@ -36,6 +42,27 @@ export default createGlobalStyle`
     ul {
         list-style: none;
     }
+
+    body .Toastify .Toastify__toast-container .Toastify__toast--success {
+  background: ${colors.successColor};
+  color: white;
+}
+ 
+.Toastify__progress-bar--success {
+  background: white;
+}
+ 
+ 
+ 
+body .Toastify .Toastify__toast-container .Toastify__toast--error {
+  background: ${colors.errorColor};
+  color: white;
+}
+
+    .Toastify__progress-bar--error {
+  background: white;
+}
+
 `;
 
 export const Container = styled.section`

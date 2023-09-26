790ef4a629635a2b056004de7b37138ad8e2fe8a
helder <helderks2104@hotmail.com>
2023-09-25 23:53:55 -0300

    Adicionado axios para consumo da API

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import { Title, Paragrafo } from './styled';
 import { Container } from '../../styles/GlobalStyles';
 
+import axios from '../../services/axios';
+
 export default function Login() {
+  React.useEffect(() => {
+    async function getData() {
+      const reponse = await axios.get('/users');
+      const { data } = reponse;
+      // eslint-disable-next-line no-console
+      console.log(data);
+    }
+
+    getData();
+  }, []);
   return (
     <Container>
       <Title>Hello World</Title>
